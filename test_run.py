529c0fc961d3174fdfddf5de05d270c27b0ac9cf
GZ <zheng.gong94@gmail.com>
2026-04-23 17:19:56 +0200

    intermed

--- test_run.py
+++ test_run.py
@@ -1,3 +1,4 @@
+from jinja2 import Environment, FileSystemLoader
 from dotenv import dotenv_values
 
 from db_client import SnowflakeApiClient, SnowflakeApiClientConfig
@@ -21,46 +22,23 @@ client = SnowflakeApiClient(
     )
 )
 
-example_sql = """
-WITH company_ids AS (
-    SELECT value::NUMBER AS company_id
-    FROM TABLE(
-        FLATTEN(INPUT => PARSE_JSON('[19049, 23013, 24937, 191564, 309845, 324490, 874652]'))
-    )
+env = Environment(loader=FileSystemLoader("sql_inventory"))
+# sql = env.get_template("financial_data_items.sql.j2").render(
+#     company_ids=[24937, 874652],
+#     data_item_id=8,
+#     date_from="2024-01-01",
+#     date_to="2025-12-31",
+# )
+sql = env.get_template("market_price.sql.j2").render(
+    company_ids=[24937, 874652],
+    date_from="2024-01-01",
+    date_to="2025-12-31",
 )
-SELECT
-    fp.COMPANYID                        AS company_id,
-    fp.fiscalYear                       AS fiscal_year,
-    fp.fiscalQuarter                    AS fiscal_quarter,
-    fp.PERIODTYPEID                     AS data_period_type_id,
-    fi.FILINGDATE                       AS reporting_date,
-    fi.RESTATEMENTTYPEID                AS restatement_type_id,
-    fi.periodEndDate                    AS fiscal_period_end,
-    fc.CURRENCYID                       AS currency_id,
-    fcd.DATAITEMVALUE                   AS data_item_value_raw,
-    fcd.UNITTYPEID                      AS unit_type_id,
-FROM CIQFINPERIOD fp
-JOIN company_ids cid
-ON fp.COMPANYID = cid.company_id
-JOIN CIQFININSTANCE fi
-ON fi.FINANCIALPERIODID = fp.FINANCIALPERIODID
-JOIN CIQFININSTANCETOCOLLECTION fic
-ON fic.FINANCIALINSTANCEID = fi.FINANCIALINSTANCEID
-JOIN CIQFINCOLLECTION fc
-ON fc.FINANCIALCOLLECTIONID = fic.FINANCIALCOLLECTIONID
-JOIN CIQFINCOLLECTIONDATA fcd
-ON fcd.FINANCIALCOLLECTIONID = fc.FINANCIALCOLLECTIONID
-WHERE
-    fcd.DATAITEMID = 8
-    AND fi.PERIODENDDATE BETWEEN '2024-01-01'
-                            AND '2025-12-31'
-;
-"""
 
 if not client.ping():
     raise RuntimeError("Snowflake connection check failed")
 
-res = client.fetch(example_sql)
+res = client.fetch(sql)
 
 df = client.res_json_to_pandas(res)
 print(df)
